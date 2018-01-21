e5648dea75ccb4547f49e2080a13474ebccf54f9
yoniffafan <yoniffafan@gmail.com>
2018-01-21 21:43:09 +0700

    change color

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,18 +10,18 @@
       <div class="form-area">
           <form role="form">
           <br style="clear:both">
-                      <h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+                      <h3 style="margin-bottom: 25px; text-align: center;">Input Data</h3>
       				<div class="form-group">
   						<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
   					</div>
   					<div class="form-group">
-  						<input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+  						<input type="text" class="form-control" id="tanggal" name="tanggal" placeholder="Tanggal" required>
   					</div>
   					<div class="form-group">
-  						<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+  						<input type="text" class="form-control" id="longi" name="longi" placeholder="longitude" required>
   					</div>
   					<div class="form-group">
-  						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+  						<input type="text" class="form-control" id="lati" name="lati" placeholder="latitude" required>
   					</div>
                       <div class="form-group">
                       <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
